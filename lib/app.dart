9107a39cdbedc72644354f151d1bff7d5bf62240
Jordan Deren <jordan.deren@privy.io>
2025-04-23 12:55:22 -0500

    Add test button for routing

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'router/app_router.dart';
 
 class AppScreen extends StatefulWidget {
   const AppScreen({super.key});
@@ -15,16 +17,21 @@ class AppScreenState extends State<AppScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: const Text("Welcome to Privy")),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text("Privy Starter Repo"),
-            ],
-          ),
+    return Scaffold(
+      appBar: AppBar(title: const Text("Welcome to Privy")),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text("Privy Starter Repo", style: TextStyle(fontSize: 24)),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                context.go(AppRouter.emailAuthPath);
+              },
+              child: const Text('Go to Email Authentication'),
+            ),
+          ],
         ),
       ),
     );
